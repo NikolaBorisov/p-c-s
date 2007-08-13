e30e9e92e47aec10ebf7c0e4a25719e6b51c0642
nikola.borisof <nikola.borisof@2d4fcf92-8237-0410-9928-31f67885ca23>
2007-08-13 05:18:08 +0000

    Some explanation added on how to compile the C extention for the Grader

--- trunk/dev/lib/pcs/ext/extconf.rb
+++ trunk/dev/lib/pcs/ext/extconf.rb
@@ -1,3 +1,5 @@
+# This is an extention to Ruby that adds methods require to grade a solution.
+# To compile the extention please execute this file with 'ruby extconf.rb'
 require 'mkmf'
 
 create_makefile("process")
