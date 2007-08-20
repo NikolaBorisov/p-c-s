a9725a58d703f55b216dd96fc77fc0eb269bb0b8
nikola.borisof <nikola.borisof@2d4fcf92-8237-0410-9928-31f67885ca23>
2007-08-20 07:29:01 +0000

    Simplifying the logging infrastucture to support only Contestants

--- trunk/dev/main/app/controllers/admin/grading_queue_controller.rb
+++ trunk/dev/main/app/controllers/admin/grading_queue_controller.rb
@@ -1,20 +1,26 @@
 class Admin::GradingQueueController < ApplicationController
-  
-  before_filter :authorize_admin
-  
+
+# FIXME: after 0.1
+#  before_filter :authorize_admin
+
   def index
     list_graders
   end
-  
+
   def start
+    # FIXME: Implement post 0.1
   end
-  
+
   def stop
+    # FIXME: Implement post 0.1
   end
-  
+
   def restart
+    # FIXME: Implement post 0.1
   end
-  
+
+  # FIXME: This method needs improvements. Should do something to test the connection to the grader.
+  # A change in the Grader, and GradingQueue is also required.
   def add_grader
     if (request.post?)
       if (params[:grader][:address]!=nil and params[:grader][:port].to_i>0)
@@ -28,7 +34,7 @@ class Admin::GradingQueueController < ApplicationController
       end
     end
   end
-  
+
   def delete_grader
     grader_id = params[:id].to_i
     connect()
@@ -36,20 +42,20 @@ class Admin::GradingQueueController < ApplicationController
     info("Grader with id #{grader_id} was deleted.", "info")
     redirect_message(:action => 'index')
   end
-  
+
   def list_graders
     connect()
     @graders_ids = @gq.get_graders
     @graders_ids.sort!
   end
-  
+
   private
-  
+
   def connect
     @gq = SOAP::RPC::Driver.new("#{$my_config[:grading_queue][:address]}:#{$my_config[:grading_queue][:port]}", $my_config[:grading_queue][:namespace])
     @gq.add_method('add_grader', 'grader_address', 'grader_port')
     @gq.add_method('get_graders')
     @gq.add_method('delete_grader', 'grader_id')
   end
-  
+
 end
--- trunk/dev/main/app/controllers/login_controller.rb
+++ trunk/dev/main/app/controllers/login_controller.rb
@@ -1,8 +1,10 @@
 class LoginController < ApplicationController
-  
+
   before_filter :authorize, :except => [:login, :signup, :register]
-  before_filter :authorize_admin, :only => [:admin]
-  
+  # Post 0.1
+#  before_filter :authorize_admin, :only => [:admin]
+
+  # Cheching the username-password combination on login
   def signup
     logged_in_user = PCS::Model::User.authenticate(params[:user][:username],params[:user][:password])
     if logged_in_user
@@ -10,22 +12,26 @@ class LoginController < ApplicationController
       logged_in_user.roles.each do |role|
         session[role.name.to_sym] = true
       end
-      
+
       redirect_to(:action => 'index')
     else
       info("Wrong username or password.", "error")
       redirect_message(:action => "login")
     end
-    
+
   end
-  
+
   def login
-    session[:user_id] = nil
-    session[:admin] = nil
-    session[:contestant] = nil
     @user = PCS::Model::User.new
   end
-  
+
+  # Cleaning up the session
+  def logout
+    session[:user_id] = nil
+    roles =  PCS::Model::Role.find(:all)
+    roles.each {|r| session[r.name.to_sym] = nil }
+  end
+
   def register
     if (request.get?)
       @user = PCS::Model::User.new
@@ -34,21 +40,20 @@ class LoginController < ApplicationController
       @user.roles << PCS::Model::Role.find_by_name("contestant")
       if @user.save
         info("User #{@user.username} registered succesfull")
-        redirect_message(:action => 'login')      
+        redirect_message(:action => 'login')
       end
     end
   end
-  
+
   def index
     @roles = PCS::Model::Role.find(:all)
   end
-  
-  def admin     
-  end
-  
+
+#  def admin
+#  end
+
   def contestant
-    redirect_to(:controller => "contestant/contest", :action => "list")
   end
-  
-  
+
+
 end
