05c352c90deb008bab6321e39e90eee8d289b425
nikola.borisof <nikola.borisof@2d4fcf92-8237-0410-9928-31f67885ca23>
2007-09-03 18:34:01 +0000

    Task controllers now require just user not admin

--- dev/main/app/controllers/admin/task/content_controller.rb
+++ dev/main/app/controllers/admin/task/content_controller.rb
@@ -1,40 +1,42 @@
 include PCS::WEB::TASK_CONTROLLER_SUPPORT
 
 class Admin::Task::ContentController < ApplicationController
+  #before_filter :autorize_admin
+  before_filter :authorize
 
- def edit()
-  task = valid_task( params[:id] )
-  redirect_to_index("Invalid task") and return unless ( task )
-  
-  @task_content = valid_content( params[:task_content_id])  
-  @task_content = PCS::Model::TaskContent.new() unless( @task_content )
-   
-  if( request.get? )
-      @languages = PCS::Model::Language.find(:all, :order => "name").map{ |lang| [lang.name, lang.id] } 
-  else    
-    @task_content.language = PCS::Model::Language.find( params[:task_content][:language_id])
-    @task_content.content = params[:task_content][:content]
-    @task_content.task = task
-    @task_content.save()
-       
-    redirect_to_task("Content saved", task.id, 4)             
+  def edit()
+    task = valid_task( params[:id] )
+    redirect_to_index("Invalid task") and return unless ( task )
+
+    @task_content = valid_content( params[:task_content_id])
+    @task_content = PCS::Model::TaskContent.new() unless( @task_content )
+
+    if( request.get? )
+      @languages = PCS::Model::Language.find(:all, :order => "name").map{ |lang| [lang.name, lang.id] }
+    else
+      @task_content.language = PCS::Model::Language.find( params[:task_content][:language_id])
+      @task_content.content = params[:task_content][:content]
+      @task_content.task = task
+      @task_content.save()
+
+      redirect_to_task("Content saved", task.id, 4)
+    end
+
   end
-  
- end
 
- def view()
-  @task_content = valid_content( params[:task_content_id] )
-  redirect_to_index("Invalid task") and return unless( @task_content )  
- end
+  def view()
+    @task_content = valid_content( params[:task_content_id] )
+    redirect_to_index("Invalid task") and return unless( @task_content )
+  end
 
- def delete()
-  task_content = valid_content( params[:id] )
-  redirect_to_index("Invalid task") and return unless( task_content )  
-  task_id = task_content.task.id  
-  
-  task_content.destroy()
-  
-  redirect_to_task("Contest deleted", task_id, 4)
- end
- 
+  def delete()
+    task_content = valid_content( params[:id] )
+    redirect_to_index("Invalid task") and return unless( task_content )
+    task_id = task_content.task.id
+
+    task_content.destroy()
+
+    redirect_to_task("Contest deleted", task_id, 4)
+  end
+
 end
--- dev/main/app/controllers/admin/task/test_controller.rb
+++ dev/main/app/controllers/admin/task/test_controller.rb
@@ -1,39 +1,40 @@
 include PCS::WEB::TASK_CONTROLLER_SUPPORT
 
 class Admin::Task::TestController < ApplicationController
-  before_filter :authorize_admin
-  
- def add()  
-   @task = valid_task( params[ :id ] )
-   number = params[:number]
+#  before_filter :authorize_admin
+  before_filter :authorize
 
-   #Handel invalid params
-   redirect_to_task("Invalid Task", params[:id], 1) and return unless( @task )    
-   redirect_to_task("Invalid Number", params[:id], 1) and return unless( is_number(number) )
-   
-   test = @task.tests.find(:first, :conditions => ["number = ?", number ] )
-   redirect_to_task("Test #{test.number} Exist", params[:id], 1) and return if( test )  
-   
-   test = PCS::Model::Test.new()
-   test.number = number
-   @task.tests << test  
-   test.save()  
-   
-   redirect_to(:action => :edit, :id => test.id)
- end
+  def add()
+    @task = valid_task( params[ :id ] )
+    number = params[:number]
 
- def edit()
-  
-  test = valid_test( params[ :id ] )  
-  redirect_to_index("Invali test") and return unless( test )    
-  
-  if ( request.get? )       
-    @test_number = test.number   
-  else   
+    #Handel invalid params
+    redirect_to_task("Invalid Task", params[:id], 1) and return unless( @task )
+    redirect_to_task("Invalid Number", params[:id], 1) and return unless( is_number(number) )
+
+    test = @task.tests.find(:first, :conditions => ["number = ?", number ] )
+    redirect_to_task("Test #{test.number} Exist", params[:id], 1) and return if( test )
+
+    test = PCS::Model::Test.new()
+    test.number = number
+    @task.tests << test
+    test.save()
+
+    redirect_to(:action => :edit, :id => test.id)
+  end
+
+  def edit()
+
+    test = valid_test( params[ :id ] )
+    redirect_to_index("Invali test") and return unless( test )
+
+    if ( request.get? )
+      @test_number = test.number
+    else
       file_inp = PCS::Model::File.save_file( params[ :file_inp ] )
       file_out = PCS::Model::File.save_file( params[ :file_out ] )
-        
-           
+
+
       if(file_inp)
         del_test_file( test, PCS::FileDescription::INPUT)
         test_file  = PCS::Model::TestFile.new()
@@ -41,45 +42,45 @@ class Admin::Task::TestController < ApplicationController
         test_file.file = file_inp
         test_file.test = test
         test_file.save()
-        
-      else   
-        #TODO: Handle the error       
-      end 
-      
-      if(file_out)  
+
+      else
+        #TODO: Handle the error
+      end
+
+      if(file_out)
         del_test_file( test, PCS::FileDescription::SOLUTION)
         test_file  = PCS::Model::TestFile.new()
         test_file.test_type = PCS::FileDescription::SOLUTION
         test_file.file = file_out
         test_file.test = test
-        test_file.save()      
-      else          
+        test_file.save()
+      else
         #TODO: Handle the error
       end
-      
-      redirect_to( :controller => 'task', :action => :edit, :id => test.task.id, :view => 1 )
-  end
-  
- end
 
- def delete()
-  test = valid_test( params[ :id ] )
-  redirect_to_index("Invalid Test") and return unless( test )
-  
-  task_id = test.task_id  
-  
-  test.destroy()
-  
-  redirect_to_task("Test Deleted!!!", task_id, 1)
- end
- 
-#Private methods
-  private  
-  
+      redirect_to( :controller => 'task', :action => :edit, :id => test.task.id, :view => 1 )
+    end
+
+  end
+
+  def delete()
+    test = valid_test( params[ :id ] )
+    redirect_to_index("Invalid Test") and return unless( test )
+
+    task_id = test.task_id
+
+    test.destroy()
+
+    redirect_to_task("Test Deleted!!!", task_id, 1)
+  end
+
+  #Private methods
+  private
+
   def del_test_file( test, type )
     test.tests_files.each { |test_file|
-      test_file.destroy() if( test_file.test_type==type )         
+      test_file.destroy() if( test_file.test_type==type )
     }
   end
- 
+
 end
