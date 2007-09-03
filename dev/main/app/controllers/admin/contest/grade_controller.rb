e1ab6663a664523dfedba0468eff66c124f41d89
nikola.borisof <nikola.borisof@2d4fcf92-8237-0410-9928-31f67885ca23>
2007-09-03 18:37:18 +0000

    Contest controllers are now useble for every user now just admin.

--- dev/main/app/controllers/admin/contest/grade_controller.rb
+++ dev/main/app/controllers/admin/contest/grade_controller.rb
@@ -1,11 +1,12 @@
 class Admin::Contest::GradeController < ApplicationController
-  
-  before_filter :authorize_admin
-  
+
+#  before_filter :authorize_admin
+  before_filter :authorize
+
   def index
     @contest = PCS::Model::Contest.find(params[:id])
   end
-  
+
   def grade_all
     submits = PCS::Model::Submit.find(:all, :conditions => ["contest_id = ?", params[:id]])
     grader_response_ids = []
@@ -14,17 +15,17 @@ class Admin::Contest::GradeController < ApplicationController
     end
     redirect_to(:action => :view_grading_queue, :ids => grader_response_ids)
   end
-  
+
   def view_grading_queue
     @grader_responses = PCS::Model::GraderResponse.find(params[:ids])
   end
-  
+
   def view_results
-    @users = PCS::Model::Users.find(:all, 
-    :joins => "INNER JOIN contests_privileges c ON constest.user_id = users.user_id", 
+    @users = PCS::Model::Users.find(:all,
+                                    :joins => "INNER JOIN contests_privileges c ON constest.user_id = users.user_id",
     :contitions => ["c.contest_id = ? and c.competed = 1)", params[:id]])
     @contest = PCS::Model::Contest.find(params[:id])
     @tasks = @contest.tasks
-    
+
   end
 end
