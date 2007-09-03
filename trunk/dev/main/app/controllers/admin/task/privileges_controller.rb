15c5ab42db24cf9a0ac11a692bb36358f7c81bde
nikola.borisof <nikola.borisof@2d4fcf92-8237-0410-9928-31f67885ca23>
2007-09-03 18:34:01 +0000

    Task controllers now require just user not admin

--- trunk/dev/main/app/controllers/admin/task/privileges_controller.rb
+++ trunk/dev/main/app/controllers/admin/task/privileges_controller.rb
@@ -1,28 +1,29 @@
 include PCS::WEB::TASK_CONTROLLER_SUPPORT
 
 class Admin::Task::PrivilegesController < ApplicationController
- before_filter :authorize_admin
- 
- def add() 
+  # before_filter :authorize_admin
+  before_filter :authorize
+
+  def add()
     task = valid_task( params[:id] )
-    user = valid_user( params[:user_id] )    
+    user = valid_user( params[:user_id] )
     redirect_to_index("Invalid data") unless( task || user)
-    
+
     user.tasks << task
-    
+
     redirect_to_task("Privileges Added", params[:id], 6)
- end
- 
- def delete()
+  end
+
+  def delete()
     task = valid_task( params[:id] )
-    user = valid_user( params[:user_id] )    
+    user = valid_user( params[:user_id] )
     redirect_to_index("Invalid data") and return unless( task || user)
-    
-    redirect_to_task("Can't delete owner", params[:id], 6) and return if( task.owner==user )    
-    
-    task.privileged_users.delete( user )    
-    
-    redirect_to_task("Privileges Deleted", params[:id], 6)    
- end
+
+    redirect_to_task("Can't delete owner", params[:id], 6) and return if( task.owner==user )
+
+    task.privileged_users.delete( user )
+
+    redirect_to_task("Privileges Deleted", params[:id], 6)
+  end
 
 end
--- trunk/dev/main/app/controllers/admin/task/restriction_controller.rb
+++ trunk/dev/main/app/controllers/admin/task/restriction_controller.rb
@@ -1,40 +1,41 @@
 include PCS::WEB::TASK_CONTROLLER_SUPPORT
 
 class Admin::Task::RestrictionController < ApplicationController
- before_filter :authorize_admin
- 
- def edit()
-  task = valid_task( params[:id] )
-  redirect_to_index("Invalid Task") and return unless( task )
-  
-  @restriction = valid_restriction( params[:restriction_id ] )  
-  @restriction = PCS::Model::Restriction.new() unless( @restriction )
-  
-  if( request.get? )    
-    
-  else
-    @restriction.runtime = params[:restriction][:runtime]
-    @restriction.memory = params[:restriction][:memory]
-    @restriction.stack_size = params[:restriction][:stack_size]    
-    @restriction.source_code = params[:restriction][:source_code]       
-    @restriction.output_size = params[:restriction][:output_size]    
-    @restriction.compilation_time = params[:restriction][:compilation_time]    
-    @restriction.task = task;   
-    @restriction.save()
-    
-    redirect_to_task("Restriction saved", task.id, 5)          
-  end
-    
- end
+#  before_filter :authorize_admin
+  before_filter :authorize
 
- def delete()
-  restriction = valid_restriction( params[:id ] )  
-  redirect_to_index("Invalid restriction") and return unless( restriction )  
-  task_id = restriction.task_id
-  
-  restriction.destroy();
-  
-  redirect_to_task("Restrictio deleted", task_id, 5)  
- end
+  def edit()
+    task = valid_task( params[:id] )
+    redirect_to_index("Invalid Task") and return unless( task )
+
+    @restriction = valid_restriction( params[:restriction_id ] )
+    @restriction = PCS::Model::Restriction.new() unless( @restriction )
+
+    if( request.get? )
+
+    else
+      @restriction.runtime = params[:restriction][:runtime]
+      @restriction.memory = params[:restriction][:memory]
+      @restriction.stack_size = params[:restriction][:stack_size]
+      @restriction.source_code = params[:restriction][:source_code]
+      @restriction.output_size = params[:restriction][:output_size]
+      @restriction.compilation_time = params[:restriction][:compilation_time]
+      @restriction.task = task;
+      @restriction.save()
+
+      redirect_to_task("Restriction saved", task.id, 5)
+    end
+
+  end
+
+  def delete()
+    restriction = valid_restriction( params[:id ] )
+    redirect_to_index("Invalid restriction") and return unless( restriction )
+    task_id = restriction.task_id
+
+    restriction.destroy();
+
+    redirect_to_task("Restrictio deleted", task_id, 5)
+  end
 
 end
